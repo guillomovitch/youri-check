3c8513e050fb292632464e2b7e38db873432ec82
Guillaume Rousse <guillomovitch@gmail.com>
2008-08-12 20:29:04 +0000

    fix default URL

--- lib/Youri/Check/Test/Updates/Source/Fedora.pm
+++ lib/Youri/Check/Test/Updates/Source/Fedora.pm
@@ -29,7 +29,7 @@ Specific parameters:
 =item url $url
 
 URL to Fedora development SRPMS directory (default:
-http://fr.rpmfind.net/linux/fedora/core/development/source/SRPMS)
+http://fr.rpmfind.net/linux/fedora/development/source/SRPMS)
 
 =back
 
@@ -38,7 +38,7 @@ http://fr.rpmfind.net/linux/fedora/core/development/source/SRPMS)
 sub _init {
     my $self    = shift;
     my %options = (
-        url => 'http://fr.rpmfind.net/linux/fedora/core/development/source/SRPMS',
+        url => 'http://fr.rpmfind.net/linux/fedora/development/source/SRPMS',
         @_
     );
 
