f085488557fbe62b811ccf288c163d433dc2061e
Guillaume Rousse <guillomovitch@gmail.com>
2008-10-12 18:37:28 +0000

    initial import

---
+++ lib/Youri/Check/WebRetriever.pm
@@ -0,0 +1,63 @@
+# $Id$
+package Youri::Check::WebRetriever;
+
+=head1 NAME
+
+Youri::Check::WebRetriever - Simple web data retriever
+
+=head1 DESCRIPTION
+
+This class provides a simple web data retriever.
+
+=cut
+
+use Moose::Policy 'Moose::Policy::FollowPBP';
+use Moose;
+use LWP::UserAgent;
+use Youri::Check::Types;
+
+has 'url'     => ( is => 'rw', isa => 'Uri' ); 
+has 'pattern' => ( is => 'rw', isa => 'RegexpRef' ); 
+has 'results' => ( is => 'rw', isa => 'HashRef' );
+
+sub BUILD {
+    my ($self, $params) = @_;
+
+    my $agent  = LWP::UserAgent->new();
+    my $results = {};
+    my $buffer = '';
+    my $callback = sub {
+        my ($data, $response, $protocol) = @_;
+
+        # prepend text remaining from previous run
+        $data = $buffer . $data;
+
+        # process current chunk
+        while ($data =~ m/(.*)\n/gc) {
+            my $line = $1;
+            next unless $line =~ $params->{pattern};
+            $results->{$1} = $2;
+        }
+
+        # store remaining text
+        my $pos = pos $data;
+        if ($pos) {
+            $buffer = substr($data, pos $data);
+        } else {
+            $buffer = $data;
+        }
+    };
+
+    $agent->get($params->{url}, ':content_cb' => $callback);
+    $self->set_results($results);
+}
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2002-2006, YOURI project
+
+This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
+
+1;
